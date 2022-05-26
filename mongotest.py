d43cbabfd5aab76152a430f98c0779da4a2747e9
kiki123meng <1197572730@qq.com>
2022-05-26 09:24:06 +0000

    12

--- mongotest.py
+++ mongotest.py
@@ -10,6 +10,8 @@ client = MongoClient('mongodb://localhost:28017/')
 #指定数据库
 db = client.local
 
-#指定集合
+#指定集合，oplog.rs是存放日志的集合
 collection = db.oplog.rs
 
+#findone()返回单个结果，find()返回一个生成器对象
+#对于find，比如说result = collection.find({'age':{'$gt':20}}),https://www.cnblogs.com/huang-yc/p/10453275.html
