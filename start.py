d22c7d089253bd3de43efdaaf5873f7659e57513
kiki123meng <1197572730@qq.com>
2022-05-26 07:47:40 +0000

    start

--- start.py
+++ start.py
@@ -1,17 +1,17 @@
 #启动
-docker run -itd -p 28017:27017 mongo --replSet rs
+#docker run -itd -p 28017:27017 mongo --replSet rs
 
 #install mongosh
 # https://www.mongodb.com/docs/mongodb-shell/install/
 
 #inti
-mongosh "mongodb://localhost:28017"
-rs.initiate()
+#mongosh "mongodb://localhost:28017"
+#rs.initiate()
 
 #find _id
-use local
-db.oplog.rs.find()
+#use local
+#db.oplog.rs.find()
 
 #运行python路径有点问题
-python /workspace/temp/pymongotest.py
+#python /workspace/temp/pymongotest.py
 
