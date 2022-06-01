53335bee8c6257f6a34400979de5b7a57cd256f0
kiki123meng <83826339+kiki123meng@users.noreply.github.com>
2022-06-02 01:22:22 +0800

    Update timedelay.py

--- timedelay.py
+++ timedelay.py
@@ -1,31 +1,31 @@
 import time
 from bson.objectid import ObjectId
-from pymongo import MongoClient, ASCENDING
+from pymongo import MongoClient, DESCENDING
 from pymongo.cursor import CursorType
 from pymongo.errors import AutoReconnect
 
 if __name__ == '__main__':
     oplog = MongoClient("mongodb://localhost:27017/?directConnection=true").local.oplog.rs
     # todo: consider rename stamp, more explicit and intutive
-    stamp = oplog.find().sort('$natural', ASCENDING).limit(-1).next()['wall']
+    curtime_stamp = oplog.find().sort('$natural', ASCENDING).limit(1).next()['wall']
 
     # todo: 过滤掉过去的数据，只计算当前时间点往后的oplog延时
     while True:
         # todo: what meaing kw? make variable names more explicit and intutive
-        kw = {}
-        kw['filter'] = {'op':'i','wall': {'$gt': stamp}, 'ns':{'$regex':'[0-9]+_TradeFlow.(TradeRecord|OrderRecord)'}}
-        kw['cursor_type'] = CursorType.TAILABLE_AWAIT
-        kw['oplog_replay'] = True
+        search_arguments = {}
+        search_argumentssearch_arguments['filter'] = {'op':'i','wall': {'$gt': curtime_stamp}, 'ns':{'$regex':'[0-9]+_TradeFlow.(TradeRecord|OrderRecord)'}}
+        search_arguments['cursor_type'] = CursorType.TAILABLE_AWAIT
+        search_arguments['oplog_replay'] = True
 
-        cursor = oplog.find(**kw)
+        cursor = oplog.find(**search_arguments)
         
         try:
             while cursor.alive:
                 for doc in cursor:
                     # todo: rename stamp to raw_data 
-                    stamp = doc['o'] 
+                    raw_data = doc['o'] 
                     # todo: rename cur
-                    cur = stamp['_id']
+                    data_id = stamp['_id']
                     src_gen_time = (cur.generation_time).replace(tzinfo=None)
                     dst_gen_time = doc['wall']
                     delay = (dst_gen_time - src_gen_time).total_seconds()
