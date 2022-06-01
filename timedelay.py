29b25c37761c6d0c89ccc44581ba5d1b7bf5477e
xujun <xujun@cloudtenfund.com>
2022-06-01 19:46:10 +0800

    review by xujun

--- timedelay.py
+++ timedelay.py
@@ -1,5 +1,4 @@
-import click
-from time import sleep
+import time
 from bson.objectid import ObjectId
 from pymongo import MongoClient, ASCENDING
 from pymongo.cursor import CursorType
@@ -10,10 +9,10 @@ if __name__ == '__main__':
     # todo: consider rename stamp, more explicit and intutive
     stamp = oplog.find().sort('$natural', ASCENDING).limit(-1).next()['wall']
 
+    # todo: 过滤掉过去的数据，只计算当前时间点往后的oplog延时
     while True:
         # todo: what meaing kw? make variable names more explicit and intutive
         kw = {}
-
         kw['filter'] = {'op':'i','wall': {'$gt': stamp}, 'ns':{'$regex':'[0-9]+_TradeFlow.(TradeRecord|OrderRecord)'}}
         kw['cursor_type'] = CursorType.TAILABLE_AWAIT
         kw['oplog_replay'] = True
@@ -27,11 +26,10 @@ if __name__ == '__main__':
                     stamp = doc['o'] 
                     # todo: rename cur
                     cur = stamp['_id']
-
                     src_gen_time = (cur.generation_time).replace(tzinfo=None)
                     dst_gen_time = doc['wall']
                     delay = (dst_gen_time - src_gen_time).total_seconds()
                     print(delay)  # todo something with doc.
-                sleep(0.1)
+                time.sleep(0.1)
         except AutoReconnect:
-            sleep(0.1)
+            time.sleep(0.1)
